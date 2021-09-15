5685f9f4d72a845bf1960e222300194cdc3760b3
LennyMar48 <lennymardiyanti48@gmail.com>
2021-09-15 16:24:11 +0700

    first commit

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,10 @@
+<?php
+class Contoh1 extends CI_Controller
+{ 
+    public function index() 
+    { 
+        echo "<h1>Perkenalkan</h1>"; 
+        echo"Nama saya Lenny Mardiyanti, Saya Mahasiswa dari Universitas Bina Sarana Informatika<br>";
+        echo "Hobi saya membaca dan olahraga yang saya sukai adalah Bulutangkis"; 
+    } 
+}
